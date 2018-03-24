c4d9f5fb3fc1b0c388410d34d1113c3d1b2470bd
wyang36 <wenqin.z.yang@gmail.com>
2018-03-24 14:20:59 -0400

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+const layout = (props) => {
+    return (
+        <Aux>
+            <div>
+                toolbar, sidedrawer, backdrop
+            </div>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Aux>
+    );
+}
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary'
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+        return (
+            <Aux>
+                <Burger></Burger>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
