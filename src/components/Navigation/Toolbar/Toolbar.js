e0fc2a8c040c8065755dc4909b64868be44b00bc
wyang36 <wenqin.z.yang@gmail.com>
2018-03-28 19:35:17 -0400

    course update
    more on navigation, side drawer and toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
 ﻿import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolBar = (props) => {
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <div>LOGO</div>
-            <nav>
-                ...
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
+            <div className={classes.Logo}>
+                <Logo></Logo>
+            </div>
+            <nav className={classes.DesktopOnly}>
+                <NavigationItems />
             </nav>
         </header>
     );
