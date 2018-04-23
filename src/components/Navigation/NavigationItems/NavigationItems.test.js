c8699f0aa96f909b0d8e5374363f7ad273a6f431
wyang36 <wenqin.z.yang@gmail.com>
2018-04-23 19:19:51 -0400

    course update
    adding automated tests

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,29 @@
+import { configure, shallow } from 'enzyme';
+import Adaptor from 'enzyme-adapter-react-16';
+import React from 'react';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adaptor() })
+
+describe('<navigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
+    it('should render 2 <NavigationItem /> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 <NavigationItem /> elements if authenticated', () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render logout <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+});
